74cf26c4242df2a68a9fd0f4b8fd4cc52b29a52a
timanfaya <timanfaya@yandex.ru>
2021-04-26 18:19:03 +0300

    init commit

---
+++ app/src/main/java/leonid/pshennikov/hometask/a1_l1/MainActivity.java
@@ -0,0 +1,14 @@
+package leonid.pshennikov.hometask.a1_l1;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
